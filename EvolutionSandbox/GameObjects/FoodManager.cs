9e80d399122e996fbb13a1e06e457e19da3fcf00
28bc23 <28bc23@proton.me>
2026-03-31 19:59:12 +0200

    add list to hold food in

--- EvolutionSandbox/GameObjects/FoodManager.cs
+++ EvolutionSandbox/GameObjects/FoodManager.cs
@@ -1,15 +1,21 @@
 using System;
+using System.Reflection.PortableExecutable;
 namespace EvolutionSandbox
 {
     internal class FoodManager : GameObject
     {
+        int MaxFood = 10;
+        List<Food> foods = new List<Food>();
         public FoodManager(Vector2Int spawnPos, Guid id) : base(spawnPos, id, 'M', GameObjectType.Food)
         {
         }
 
         public override void Update()
         {
-            
+            if (foods.Count < MaxFood)
+            {
+                //TODO: Spawn food
+            }
         }
     }
 }
