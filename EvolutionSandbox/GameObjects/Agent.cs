60ef937b668eb59afd8565940b2e396576f16818
28bc23 <28bc23@proton.me>
2026-04-03 00:52:11 +0200

    add energy decresing by EnergyDecreseRate every second

--- EvolutionSandbox/GameObjects/Agent.cs
+++ EvolutionSandbox/GameObjects/Agent.cs
@@ -4,12 +4,17 @@ namespace EvolutionSandbox
 {
     internal class Agent : GameObject
     {
+        double Energy = 100;
+        float EnergyDecreaseRate = 1;
         public Agent(Vector2Int spawnPos, Guid id) : base(spawnPos, id, '*', GameObjectType.Agent)
         {
         }
 
-        public override void Update()
+        public override void Update(double deltaTime)
         {
+            //Desrease energy
+            Energy -= EnergyDecreaseRate * deltaTime;
+
             //Movement test
             MovementType randomMove = (MovementType)Program.GRND.Next(0, 12);
             MakeAction(new MoveAction(randomMove, GSPos, this));
